008ae12d5b8e66be4ee2c2a125dc3981e38c5c36
zhco00 <85988984+zhco00@users.noreply.github.com>
2022-08-22 19:23:53 +0900

    react-2week

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Register from "./components/register";
 import Login from "./components/Login";
+import Introduce from "./components/Introduce";
 function App() {
   return (
     <Router>
       <div>
         <ul>
+          <Link to={"/"}>
+            <li>Introduce</li>
+          </Link>
           <Link to={"/login"}>
             <li>Login</li>
           </Link>
@@ -17,6 +21,7 @@ function App() {
         </ul>
 
         <Routes>
+          <Route path="/" element={<Introduce />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
---
+++ src/components/Introduce.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+export default function Introduce() {
+  return (
+    <div>
+      <h2>안녕하세요 한민규 홈페이지 입니다.</h2>
+    </div>
+  );
+}
